972a247a3d0db7e64ac2fcdc4d9695c7bc4f252c
Saw <77043862+jigsaw-4277821@users.noreply.github.com>
2022-02-28 11:52:48 +0200

    Update SUtil.hx

--- source/SUtil.hx
+++ source/SUtil.hx
@@ -64,12 +64,12 @@ class SUtil
         }
 
         if (!FileSystem.exists(SUtil.getPath() + "assets")){
-            SUtil.applicationAlert("Instructions:", "Try copying assets/assets from apk to your internal storage app directory " + "( here " + SUtil.getPath() + " )" + "if you hadn't have Zarhiver Downloaded, download it and enable the show hidden files option to have the folder visible" + "\n" + "Press Ok To Close The App");
+            SUtil.applicationAlert("Instructions:", "You have to copy assets/assets from apk to your internal storage app directory " + "( here " + SUtil.getPath() + " )" + "if you hadn't have Zarhiver Downloaded, download it and enable the show hidden files option to have the folder visible" + "\n" + "Press Ok To Close The App");
 	    flash.system.System.exit(0);
         }
         
         if (!FileSystem.exists(SUtil.getPath() + "mods")){
-            SUtil.applicationAlert("Instructions:", "Try copying assets/mods from apk to your internal storage app directory " + "( here " + SUtil.getPath() + " )" + "if you hadn't have Zarhiver Downloaded, download it and enable the show hidden files option to have the folder visible" + "\n" + "Press Ok To Close The App");
+            SUtil.applicationAlert("Instructions:", "You have to copy assets/mods from apk to your internal storage app directory " + "( here " + SUtil.getPath() + " )" + "if you hadn't have Zarhiver Downloaded, download it and enable the show hidden files option to have the folder visible" + "\n" + "Press Ok To Close The App");
 	    flash.system.System.exit(0);
         }
         #end
@@ -82,53 +82,49 @@ class SUtil
      
     static public function onCrash(e:UncaughtErrorEvent):Void {
         var callStack:Array<StackItem> = CallStack.exceptionStack(true);
-		var dateNow:String = Date.now().toString();
-		dateNow = StringTools.replace(dateNow, " ", "_");
-		dateNow = StringTools.replace(dateNow, ":", "'");
-		var path:String = "log/" + "crash_" + dateNow + ".txt";
+	var dateNow:String = Date.now().toString();
+	dateNow = StringTools.replace(dateNow, " ", "_");
+	dateNow = StringTools.replace(dateNow, ":", "'");
+	var path:String = "log/" + "crash_" + dateNow + ".txt";
 
-		var errMsg:String = "";
+	var errMsg:String = "";
 
-		for (stackItem in callStack)
+	for (stackItem in callStack)
+	{
+		switch (stackItem)
 		{
-			switch (stackItem)
-			{
-				case FilePos(s, file, line, column):
-					errMsg += file + " (line " + line + ")\n";
-				default:
-					Sys.println(stackItem);
-			}
+			case FilePos(s, file, line, column):
+				errMsg += file + " (line " + line + ")\n";
+			default:
+				Sys.println(stackItem);
 		}
-
-                errMsg += e.error;
-
-                if (!FileSystem.exists(SUtil.getPath() + "log")){
-                    FileSystem.createDirectory(SUtil.getPath() + "log");
-                }
-
-		File.saveContent(SUtil.getPath() + path, errMsg + "\n");
-
-		Sys.println(errMsg);
-		Sys.println("Crash dump saved in " + Path.normalize(path));
-		Sys.println("Making a simple alert ...");
-		
-		SUtil.applicationAlert("Uncaught Error:", errMsg);
-
-		flash.system.System.exit(0);
 	}
+
+        errMsg += e.error;
+
+        if (!FileSystem.exists(SUtil.getPath() + "log")){
+            FileSystem.createDirectory(SUtil.getPath() + "log");
+        }
+
+        File.saveContent(SUtil.getPath() + path, errMsg + "\n");
+
+	Sys.println(errMsg);
+	Sys.println("Crash dump saved in " + Path.normalize(path));
+	Sys.println("Making a simple alert ...");
+		
+	SUtil.applicationAlert("Uncaught Error:", errMsg);
+	flash.system.System.exit(0);
+    }
 	
     public static function applicationAlert(title:String, description:String){
         Application.current.window.alert(description, title);
     }
 
-    static public function saveContent(fileName:String = "file", fileExtension:String = ".json", fileData:String = "you forgot something to add in your editor"){
+    static public function saveContent(fileName:String = "file", fileExtension:String = ".json", fileData:String = "you forgot something to add in your code"){
         if (!FileSystem.exists(SUtil.getPath() + "system-saves")){
             FileSystem.createDirectory(SUtil.getPath() + "system-saves");
         }
 
         sys.io.File.saveContent(SUtil.getPath() + "system-saves" + fileName + fileExtension, fileData);
-        #if android
-        android.AndroidTools.toast("File Saved Successfully!!");
-        #end
     }
 }
