73015a1d5db507903a27ea5afff40845c365dfa6
jigsaw-4277821 <mihaialexandru427@yahoo.com>
2022-09-03 13:34:25 +0300

    Thanks for the idea poyo!

--- source/android/flixel/FlxHitbox.hx
+++ source/android/flixel/FlxHitbox.hx
@@ -5,8 +5,6 @@ import flash.display.Shape;
 import flash.display.BitmapData;
 import flixel.FlxG;
 import flixel.util.FlxDestroyUtil;
-import flixel.tweens.FlxTween;
-import flixel.tweens.FlxEase;
 import flixel.group.FlxSpriteGroup;
 
 /**
@@ -65,7 +63,6 @@ class FlxHitbox extends FlxSpriteGroup
 
 	private function createHint(X:Float, Y:Float, Width:Int, Height:Int, Color:Int = 0xFFFFFF):FlxButton
 	{
-		var hintTween:FlxTween = null;
 		var hint:FlxButton = new FlxButton(X, Y);
 		hint.loadGraphic(createHintGraphic(Width, Height, Color));
 		hint.solid = false;
@@ -74,42 +71,18 @@ class FlxHitbox extends FlxSpriteGroup
 		hint.alpha = 0.00001;
 		hint.onDown.callback = function()
 		{
-			if (hintTween != null)
-				hintTween.cancel();
-
-			hintTween = FlxTween.tween(hint, {alpha: AndroidControls.getOpacity(true)}, AndroidControls.getOpacity(true) / 100, {
-				ease: FlxEase.circInOut,
-				onComplete: function(twn:FlxTween)
-				{
-					hintTween = null;
-				}
-			});
+			if (hint.alpha != AndroidControls.getOpacity(true))
+				hint.alpha = AndroidControls.getOpacity(true);
 		}
 		hint.onUp.callback = function()
 		{
-			if (hintTween != null)
-				hintTween.cancel();
-
-			hintTween = FlxTween.tween(hint, {alpha: 0.00001}, AndroidControls.getOpacity(true) / 10, {
-				ease: FlxEase.circInOut,
-				onComplete: function(twn:FlxTween)
-				{
-					hintTween = null;
-				}
-			});
+			if (hint.alpha != 0.00001)
+				hint.alpha = 0.00001;
 		}
 		hint.onOut.callback = function()
 		{
-			if (hintTween != null)
-				hintTween.cancel();
-
-			hintTween = FlxTween.tween(hint, {alpha: 0.00001}, AndroidControls.getOpacity(true) / 10, {
-				ease: FlxEase.circInOut,
-				onComplete: function(twn:FlxTween)
-				{
-					hintTween = null;
-				}
-			});
+			if (hint.alpha != 0.00001)
+				hint.alpha = 0.00001;
 		}
 		#if FLX_DEBUG
 		hint.ignoreDrawDebug = true;
